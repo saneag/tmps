b889230ab21da608270ff55dce8f2ffd7093eb58
A13x <69859268+saneag@users.noreply.github.com>
2023-04-21 22:50:30 +0300

    [UI] Finish login/register page (#1)

--- proiect-de-curs/frontend/src/index.tsx
+++ proiect-de-curs/frontend/src/index.tsx
@@ -8,6 +8,7 @@ import { store } from 'redux/store';
 import './styles/styles.scss';
 
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
