96caebecce1ae88dfb1c9fd9fcc61ef01f5636c3
A13x <69859268+saneag@users.noreply.github.com>
2023-04-21 22:50:30 +0300

    [UI] Finish login/register page (#1)

--- proiect-de-curs/frontend/tailwind.config.js
+++ proiect-de-curs/frontend/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
     content: ['./src/**/*.{js,jsx,ts,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            spacing: {
+                128: '32rem',
+            },
+        },
         container: {
             center: true,
         },
